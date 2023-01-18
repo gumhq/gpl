3a54dfa9136ad88308cc5dafe1f91eef41d99546
vjgpt <vjgupta57@gmail.com>
2023-01-18 16:53:12 +0000

    adding testcases

---
+++ tests/post.spec.ts
@@ -0,0 +1,61 @@
+import * as anchor from "@project-serum/anchor";
+import randombytes from "randombytes";
+import { expect } from "chai";
+import { GplCore } from "../target/types/gpl_core";
+
+const program = anchor.workspace.GplCore as anchor.Program<GplCore>;
+
+anchor.setProvider(anchor.AnchorProvider.env());
+
+describe("Post", async () => {
+  let userPDA: anchor.web3.PublicKey;
+  let profilePDA: anchor.web3.PublicKey;
+  let postPDA: anchor.web3.PublicKey;
+
+  before(async () => {
+    // Create a user
+    const randomHash = randombytes(32);
+    const userTx = program.methods.createUser(randomHash);
+    const userPubKeys = await userTx.pubkeys();
+    userPDA = userPubKeys.user as anchor.web3.PublicKey;
+    await userTx.rpc();
+
+    // Create a profile
+    const profileTx = program.methods.createProfile("Personal").accounts({ user: userPDA });
+    const profilePubKeys = await profileTx.pubkeys();
+    profilePDA = profilePubKeys.profile as anchor.web3.PublicKey;
+    await profileTx.rpc();
+  });
+
+  it("should create a post", async () => {
+    const randomHash = randombytes(32);
+    const metadataUri = "This is a test post";
+    const post = program.methods.createPost(metadataUri, randomHash).accounts({ user: userPDA, profile: profilePDA });
+    const postPubKeys = await post.pubkeys();
+    postPDA = postPubKeys.post as anchor.web3.PublicKey;
+    await post.rpc();
+    const postAccount = await program.account.post.fetch(postPDA);
+    expect(postAccount.metadataUri).is.equal(metadataUri);
+    expect(postAccount.profile.toString()).is.equal(profilePDA.toString());
+  });
+
+  it("should update a post", async () => {
+    const metadataUri = "This is an updated test post";
+    const post = program.methods.updatePost(metadataUri).accounts({ user: userPDA, profile: profilePDA, post: postPDA });
+    await post.rpc();
+    const postAccount = await program.account.post.fetch(postPDA);
+    expect(postAccount.metadataUri).is.equal(metadataUri);
+    expect(postAccount.profile.toString()).is.equal(profilePDA.toString());
+  });
+
+  it("should delete a post", async () => {
+    const post = program.methods.deletePost().accounts({ user: userPDA, profile: profilePDA, post: postPDA });
+    await post.rpc();
+    try {
+      await program.account.post.fetch(postPDA);
+    } catch (error: any) {
+      expect(error).to.be.an("error");
+      expect(error.toString()).to.contain(`Account does not exist ${postPDA.toString()}`);
+    }
+  });
+});
---
+++ tests/profile.spec.ts
@@ -0,0 +1,43 @@
+import * as anchor from "@project-serum/anchor";
+import randombytes from "randombytes";
+import { expect } from "chai";
+import { GplCore } from "../target/types/gpl_core";
+
+const program = anchor.workspace.GplCore as anchor.Program<GplCore>;
+
+anchor.setProvider(anchor.AnchorProvider.env());
+
+describe("Profile", async () => {
+  let userPDA: anchor.web3.PublicKey;
+  let profilePDA: anchor.web3.PublicKey;
+
+  before(async () => {
+    // Create a user
+    const randomHash = randombytes(32);
+    const tx = program.methods.createUser(randomHash);
+    const pubKeys = await tx.pubkeys();
+    userPDA = pubKeys.user as anchor.web3.PublicKey;
+    await tx.rpc();
+  });
+
+  it("should create a profile", async () => {
+    const tx = program.methods.createProfile("Personal").accounts({ user: userPDA });
+    const pubKeys = await tx.pubkeys();
+    profilePDA = pubKeys.profile as anchor.web3.PublicKey;
+    await tx.rpc();
+    const profileAccount = await program.account.profile.fetch(profilePDA);
+    expect(profileAccount.user.toString()).is.equal(userPDA.toString());
+    expect(profileAccount.namespace.toString()).is.equal({ personal: {} }.toString());
+  });
+
+  it("should delete a profile", async () => {
+    const tx = program.methods.deleteProfile().accounts({ user: userPDA, profile: profilePDA });
+    await tx.rpc();
+    try {
+      await program.account.profile.fetch(profilePDA);
+    } catch (error: any) {
+      expect(error).to.be.an("error");
+      expect(error.toString()).to.contain(`Account does not exist ${profilePDA.toString()}`);
+    }
+  });
+});
