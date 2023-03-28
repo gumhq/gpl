fbbe82ce3d3bcb1957c1d9b53ed2ce7de2b96f42
vjgpt <vjgupta57@gmail.com>
2023-03-28 11:22:29 +0000

    test: add testcases to check if session token can be used by wrong authority

--- tests/gpl_core/post.spec.ts
+++ tests/gpl_core/post.spec.ts
@@ -2,7 +2,8 @@ import * as anchor from "@project-serum/anchor";
 import randombytes from "randombytes";
 import { expect } from "chai";
 import { GplCore } from "../../target/types/gpl_core";
-import { new_session } from "../utils";
+import { airdrop, new_session } from "../utils";
+import { sendAndConfirmTransaction } from "@solana/web3.js";
 
 const program = anchor.workspace.GplCore as anchor.Program<GplCore>;
 
@@ -80,13 +81,59 @@ describe("Post", async () => {
   });
 
   describe("Post with session token", async () => {
+    let rpcConnection: anchor.web3.Connection;
     let sessionToken: anchor.web3.PublicKey;
     let sessionKeypair: anchor.web3.Keypair;
+    let randomUser: anchor.web3.Keypair;
+    let randomUserWallet: anchor.Wallet;
+    let randomUserPDA: anchor.web3.PublicKey;
+    let randomProfilePDA: anchor.web3.PublicKey;
+    let randomPostPDA: anchor.web3.PublicKey;
 
     before(async () => {
+      rpcConnection = new anchor.web3.Connection(
+        "http://localhost:8899",
+        "confirmed"
+      );
+      // Create a random user
+      randomUser = anchor.web3.Keypair.generate();
+      randomUserWallet = new anchor.Wallet(randomUser);
+      await airdrop(randomUser.publicKey);
+
+      // Create a user
+      const randomHash = randombytes(32);
+      const userTx = program.methods.createUser(randomHash)
+        .accounts({ authority: randomUser.publicKey });
+      const userPubKeys = await userTx.pubkeys();
+      randomUserPDA = userPubKeys.user;
+      const tx = await userTx.transaction();
+      tx.recentBlockhash = (await rpcConnection.getRecentBlockhash()).blockhash;
+      tx.feePayer = randomUser.publicKey;
+      const signedTestUserTransaction = await randomUserWallet.signTransaction(tx);
+      await sendAndConfirmTransaction(rpcConnection, signedTestUserTransaction, [randomUser]);
+
+      // Create a profile
+      const testProfile = program.methods
+        .createProfile("Personal")
+        .accounts({ user: randomUserPDA, authority: randomUser.publicKey });
+      const testProfilePubKeys = await testProfile.pubkeys();
+      randomProfilePDA = testProfilePubKeys.profile as anchor.web3.PublicKey;
+      const testProfileTx = await testProfile.transaction();
+      testProfileTx.recentBlockhash = (
+        await rpcConnection.getLatestBlockhash()
+      ).blockhash;
+      testProfileTx.feePayer = randomUser.publicKey;
+      const signedTransaction = await randomUserWallet.signTransaction(
+        testProfileTx
+      );
+      await sendAndConfirmTransaction(rpcConnection, signedTransaction, [
+        randomUser,
+      ]);
+
+      // Create a session
       // @ts-ignore
       const { sessionPDA, sessionSigner } = await new_session(
-        provider.wallet.publicKey,
+        provider.publicKey,
         program.programId
       );
       sessionToken = sessionPDA;
@@ -110,6 +157,54 @@ describe("Post", async () => {
       const postAccount = await program.account.post.fetch(postPDA);
       expect(postAccount.metadataUri).is.equal(metadataUri);
       expect(postAccount.profile.toString()).is.equal(profilePDA.toString());
+    });
+
+    it("should not create a post with sessionToken created by wrong authority", async () => {
+      const randomHash = randombytes(32);
+      const metadataUri = "This is a test post";
+      const post = program.methods
+        .createPost(metadataUri, randomHash)
+        .accounts({
+          user: randomUserPDA,
+          profile: randomProfilePDA,
+          sessionToken: sessionToken,
+          authority: randomUser.publicKey,
+        });
+      const postPubKeys = await post.pubkeys();
+      randomPostPDA = postPubKeys.post as anchor.web3.PublicKey;
+      try {
+        await post.signers([randomUser]).rpc();
+      } catch (error: any) {
+        console.log(error);
+        expect(error).to.be.an("error");
+        expect(error.toString()).to.contain(
+          "Error Code: ConstraintRaw"
+        );
+      }
+    });
+
+    it("should not create a post with wrong authority", async () => {
+      const randomHash = randombytes(32);
+      const metadataUri = "This is a test post";
+      const post = program.methods
+        .createPost(metadataUri, randomHash)
+        .accounts({
+          user: randomUserPDA,
+          profile: randomProfilePDA,
+          sessionToken: sessionToken,
+          authority: randomUser.publicKey,
+        });
+      const postPubKeys = await post.pubkeys();
+      randomPostPDA = postPubKeys.post as anchor.web3.PublicKey;
+      try {
+        await post.signers([sessionKeypair]).rpc();
+      } catch (error: any) {
+        console.log(error);
+        expect(error).to.be.an("error");
+        expect(error.toString()).to.contain(
+          "Error: unknown signer"
+        );
+      }
     });
 
     it("should update a post", async () => {
